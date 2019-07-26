5e8265495cf2eea9fedcf61ba6b38527dbcea520
eers1 <eers@leeds.ac.uk>
2019-07-26 10:40:00 +0100

    Added profiles script and neatened function

--- profiles.py
+++ profiles.py
@@ -6,9 +6,10 @@ import matplotlib.pyplot as plt
 nc = '../../MONC/dycoms_simulation/mbl_sc_dycoms_dg_7260.0.nc'
 DS = xr.open_dataset(nc)
 
-def scalar_plot(obj, name):
-    obj.plot()
+def profile_plots(DS, obj, name):
+    var_T = obj.transpose()
+    var_T.plot()
     plt.title(name, fontsize=12)
     plt.show()
 
-[scalar_plot(obj, name) for (name, obj) in DS.data_vars.items() if len(obj.dims) == 1] 
+[profile_plots(DS, obj, name) for (name, obj) in DS.data_vars.items() if len(obj.dims) == 2] 
