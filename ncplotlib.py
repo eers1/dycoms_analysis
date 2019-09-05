8a715f4928200112bb30694c541afb0d07dce642
eers1 <45171613+eers1@users.noreply.github.com>
2019-09-05 10:29:45 +0100

    Format edit

--- ncplotlib.py
+++ ncplotlib.py
@@ -1,30 +1,30 @@
 # Library of functions for plotting straight from netCDF files
 import matplotlib.pyplot as plt
 
-# Plot scalar diagnostics at each simulation time
 def scalar(var):
+    # Plot scalar diagnostics at each simulation time
     var.plot()
     plt.title(var.name, fontsize=12)
     plt.show()
 
 
-# Plot profile diagnostics - through height and at each simulation time
 def profile(var):
+    # Plot profile diagnostics - through height and at each simulation time
     var_T = var.transpose()
     var_T.plot()
     plt.title(var.name, fontsize=12)
     plt.show()
 
-
-# Plot scene diagnostics - x and y domain lengths averaged over simulation time
+    
 def scene(var):
+    # Plot scene diagnostics - x and y domain lengths averaged over simulation time
     var.plot(x='x', y='y', col=var.dims[0])
     plt.title(var.name, fontsize=12)
     plt.show()
 
 
-# Plot vertical slice through 4D diagnostics - x or y vs height at time snapshots
 def vslice(var, horizontal_dim, depth):
+    # Plot vertical slice through 4D diagnostics - x or y vs height at time snapshots
     time = var.dims[0]
     z = var.dims[3]
     if horizontal_dim == 'x':
@@ -35,8 +35,8 @@ def vslice(var, horizontal_dim, depth):
     plt.show()
 
 
-# Plot horizontal slice through 4D diagnostics - x vs y at specific height at time snapshots
 def hslice(var, height):
+    # Plot horizontal slice through 4D diagnostics - x vs y at specific height at time snapshots
     time = var.dims[0]
     if var.dims[3] == 'z':
         var.sel(z=height, method='nearest').plot(x='x', y='y', col=time)
