739daeb116200ee87a6ca441a4e985dc6c3083d3
Rachel Sansom <eers@leeds.ac.uk>
2019-09-10 13:53:50 +0100

    Function added for short runs when slicing time series is not necessary.

--- core_plots.py
+++ core_plots.py
@@ -5,13 +5,13 @@ import matplotlib.pyplot as plt
 import ncplotlib as ncplt
 
 # Open dataset
-nc = '/work/xfc/vol1/user_cache/eers/dycoms_diurnal_test/merged.nc'
+nc = '../../mbl_sc_casim_dg_7260_test2.nc'
 DS = xr.open_dataset(nc)
 
 # Select specific diagnostics or take all
 names_1D = ['VWP_mean', 'LWP_mean', 'RWP_mean', 'reske_mean', 'subke_mean', 'w_max', 'senhf_mean', 'lathf_mean', 'surface_precip_mean']
 names_2D = ['theta_mean', 'vapour_mmr_mean', 'liquid_mmr_mean', 'rain_mmr_mean', 'rh_mean', 'subgrid_turbulent_transport', 'total_cloud_fraction', 'liquid_cloud_fraction', 'resolved_turbulent_transport', 'tke_tendency','u_wind_mean', 'v_wind_mean', 'w_wind_mean', 'uu_mean', 'vv_mean', 'ww_mean']
-names_3D = ['surface_precip', 'vwp', 'lwp', 'rwp', 'reske', 'subke', 'cltop', 'clbas', 'toa_up_longwave', 'toa_down_shortwave', 'toa_up_shortwave']
+names_3D = ['surface_precip', 'vwp', 'lwp', 'rwp', 'reske', 'subke', 'cltop', 'clbas'] # 'toa_up_longwave', 'toa_down_shortwave', 'toa_up_shortwave']
 names_4D = [key for (key, val) in DS.data_vars.items() if len(val.dims) == 4]
 
 # Create object list
@@ -21,7 +21,7 @@ scenes = [DS[name] for name in names_3D]
 slices = [DS[name] for name in names_4D]
 
 # Plot using ncplotlib
-savepath = '../../save/'
+savepath = '../../testplots1/'
 
 for var in scalars:
     fig, axes = plt.subplots()
@@ -35,11 +35,12 @@ for var in profiles:
 
 #for var in scenes:
 #    #plt.subplots()
-#    ncplt.scene(var, 10)
-#    plt.savefig(savepath + var.name + '.png')
-#   print(var.name + ' saved')
+#    ncplt.scene(var, 10, savepath)
+#    print(var.name + ' saved')
 #
 #print('*** scenes processed ***')
-[ncplt.scene(var,5) for var in scenes]
-[ncplt.vslice(var, 'x', 60, 5) for var in slices]
 
+   
+[ncplt.scene(var, savepath) for var in scenes]
+[ncplt.vslice(var, 'x', 60, savepath) for var in slices]
+                                   
--- ncplotlib.py
+++ ncplotlib.py
@@ -7,6 +7,7 @@ def scalar(fig, axes, var):
     plt.title(var.name, fontsize=12)
     return fig
 
+
 def profile(fig, axes, var):
     # Plot profile diagnostics - through height and at each simulation time
     var_T = var.transpose()
@@ -14,30 +15,61 @@ def profile(fig, axes, var):
     plt.title(var.name, fontsize=12)
     return fig
 
+
+def scene(var, savepath):
+    # Plot scene diagnostics - x and y domain lengths averaged over height at time snapshots. For short simulations, quick plotting
+    t = var.dims[0] # time series
+    var.plot(x='x', y='y', col=t, col_wrap=5)
+    plt.title(var.name, fontsize=12)
+    plt.savefig(savepath + var.name + '.png')
+
     
-def scene(var, num_plots):
+def scene_long(var, num_plots, savepath):
     # Plot scene diagnostics - x and y domain lengths averaged over simulation time
     t = var.dims[0] # time series
     tlast = (var.sizes[t]-1)
     var.isel(**{t:slice(0, tlast, int(tlast/num_plots))}).plot(x='x',y='y', col=t, col_wrap=5)
     plt.title(var.name, fontsize=12)
-    plt.show() 
+    plt.savefig(savepath + var.name + '.png')
 
 
-def vslice(var, horizontal_dim, depth, num_plots):
+def vslice(var, depth, horizontal_dim, savepath):
+    # Plot vertical slice through 4D diagnostics - x or y vs height at time snapshots. For SHORT simulations, quick plotting
+    t = var.dims[0] # time series
+    z = var.dims[3]
+    if horizontal_dim == 'x':
+        var[:,depth,:,:].plot(x='y', y=z, col=t, col_wrap=5)
+    elif horizontal_dim == 'y':
+        var[:,:,depth,:].plot(x='x', y=z, col=t, col_wrap=5)
+    plt.title(var.name, fontsize=12)
+    plt.savefig(savepath + var.name + '.png')
+
+
+def vslice_long(var, horizontal_dim, depth, num_plots, savepath):
     # Plot vertical slice through 4D diagnostics - x or y vs height at time snapshots
     t = var.dims[0] # time series
     tlast = (var.sizes[t]-1)
     z = var.dims[3]
     if horizontal_dim == 'x':
-        var[:,depth,:,:].isel(**{t:slice(0, tlast, int(tlast/num_plots))}).plot(x='y',y=z, col=t, col_wrap=5)
+        var[:,depth,:,:].isel(**{t:slice(0, tlast, int(tlast/num_plots))}).plot(x='y', y=z, col=t, col_wrap=5)
     elif horizontal_dim == 'y':
-        var[:,:,depth,:].isel(**{t:slice(0, tlast, int(tlast/num_plots))}).plot(x='x',y=z, col=t, col_wrap=5)
+        var[:,:,depth,:].isel(**{t:slice(0, tlast, int(tlast/num_plots))}).plot(x='x', y=z, col=t, col_wrap=5)
     plt.title(var.name, fontsize=12)
-    plt.show()
+    plt.savefig(savepath + var.name + 'long.png')
 
 
-def hslice(var, height, num_plots):
+def hslice(var, height, savepath):
+    # Plot horizontal slice through 4D diagnostics - x vs y at specific height at time snapshots. For SHORT simulations, quick plotting.
+    t = var.dims[0] # time series
+    if var.dims[3] == 'z':
+        var.sel(z=height, method='nearest').plot(x='x', y='y', col=t, col_wrap=5)
+    else:
+        var.sel(zn=height, method='nearest').plot(x='x', y='y', col=t, col_wrap=5)
+    plt.title(var.name, fontsize=12)
+    plt.savefig(savepath + var.name + '.png')
+
+
+def hslice_long(var, height, num_plots, savepath):
     # Plot horizontal slice through 4D diagnostics - x vs y at specific height at time snapshots
     t = var.dims[0] # time series
     tlast = (var.sizes[t]-1)
@@ -46,4 +78,4 @@ def hslice(var, height, num_plots):
     else:
         var.isel(**{t:slice(0, tlast, int(tlast/num_plots))}).sel(zn=height, method='nearest').plot(x='x', y='y', col=t, col_wrap=5)
     plt.title(var.name, fontsize=12)
-    plt.show()
+    plt.savefig(savepath + var.name + 'long.png')
