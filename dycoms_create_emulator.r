610a1dfd46738cbe656007aaee81ec7131504c81
eers1 <eers@leeds.ac.uk>
2024-05-16 12:01:47 +0100

    Tidied up R code

--- dycoms_create_emulator.r
+++ dycoms_create_emulator.r
@@ -32,15 +32,18 @@ krig.mean = function(Xnew,m) {
 #### Main ####
 ################################################################################
 # Load data and create variables -----------------------------------------------
+# File extensions
 nd   <- "low"
 calc <- "cloud_frac" # lwp_cloud cloud_frac
-type <- "mean"  # mean teme
-### lwp_cloud teme: 1mag is 50 and 2mag is 10
-data_path  <- sprintf("../Data_csv/%s_nd/dycoms_data_%s_nd_%s_%s.csv", nd, nd, calc, type)
-DycomsData <- read.csv(data_path,header=FALSE,stringsAsFactors=FALSE)
+type <- "mean"       # mean teme
+noise_type <- "unnormal_2_ungrouped"
 
-noise_type <- "extras"
-multiplier <- "none"
+# indices of the ensembles that are being used:
+iterator<-list(2,5)     # list(0,1,2,3,4,5,6,7,8)
+
+# Load data
+data_path  <- sprintf("data_%s/dycoms_data_%s_nd_%s_%s.csv", calc, nd, calc, type)
+DycomsData <- read.csv(data_path,header=FALSE,stringsAsFactors=FALSE)
 
 # Create parameter table
 Min <- c(2,-9)
@@ -56,6 +59,12 @@ if (noise_type=="exact") {
 } else {
   training_input <- rbind(DycomsData[1:20,1:2],DycomsData[33:38,1:2])
 }
+
+# Make unit
+training_input[,1] <- (training_input[,1] - DesignRangesTable$Min[1])/(DesignRangesTable$Max[1] - DesignRangesTable$Min[1])
+training_input[,2] <- (training_input[,2] - DesignRangesTable$Min[2])/(DesignRangesTable$Max[2] - DesignRangesTable$Min[2])
+validation_input[,1] <- (validation_input[,1] - DesignRangesTable$Min[1])/(DesignRangesTable$Max[1] - DesignRangesTable$Min[1])
+validation_input[,2] <- (validation_input[,2] - DesignRangesTable$Min[2])/(DesignRangesTable$Max[2] - DesignRangesTable$Min[2])
 
 n_training   <- length(training_input[,1])
 n_validation <- length(validation_input[,1])
@@ -88,52 +97,26 @@ pairs(cbind(rbind(training_input, validation_input),lwp=c(training_output, valid
 # If using initial-condition ensembles, replace the training data at the 
 # ensemble points with the mean
 if (noise_type!="exact" & noise_type!="extras" & noise_type!="1mag" & noise_type!="2mag" & noise_type!="trial") {
-  ensembleData<-read.csv(sprintf("/home/rach/Emulator/Ensembles/ensemble_%s_mean.csv", calc),header=FALSE,stringsAsFactors=FALSE)
-  
-  if (type=="mean") {
-    ensemble<-ensembleData[,1]
-  } else {
-    ensemble<-ensembleData[,2]
-  }
-  
-  if (grepl("012", noise_type)) {
-    iterator<-list(6,2,3)
-  } else if (grepl("048", noise_type)) {
-    iterator<-list(6,0,7)
-  } else if (grepl("678", noise_type)) {
-    iterator<-list(4,5,7)
-  } else if (noise_type=="two_regime") {
-    iterator<-list(0, 3)  # 2,4
-  } else {
-    iterator<-list(0,1,2,3,4,5,6,7,8)
-  }
   indices<-c(3,9,11,14,15,17,18,19,20)
   
-  for (j in iterator) {
-    print(j)
-    training_output[indices[j+1]]<-mean(ensemble[(j*5+1):(j*5+5)])
+  ensembleData<-read.csv(sprintf("data_%s/ensemble_%s_mean.csv", calc, calc),
+                         header=FALSE,stringsAsFactors=FALSE)
+  if (type=="mean") {
+      ensemble<-ensembleData[,1]
+  } else {
+      ensemble<-ensembleData[,2]
   }
-}  
+  
+  for (j in iterator) {
+    training_output[indices[j+1]]<-mean(ensemble[(j*5+1) : (j*5+5)])
+  }
+}
 
 # Load noise vector 
 rm(NV)
-if (noise_type=="1mag" | noise_type=="2mag") {
-  noise_read = sprintf("/home/rach/Emulator/Noise_files/residual_variance/%s/nv_normal_res_%s.csv", 
-                       calc, noise_type)
-  noise_vector<-read.csv(noise_read,header=FALSE,stringsAsFactors=FALSE)
-  NV<-c(noise_vector[1:20,1],noise_vector[33:38,1])
-} else if (noise_type=="trial"){
-    NV<-rep(0.000043, n_training)   # lwp_teme approx 2.5 (max=5) # cf_mean = 0.001 (max=0.01) # cf_teme 0.000043
-    #noise_read = sprintf("/home/rach/Emulator/Noise_files/residual_variance/%s/nv_normal_res_%s_unnormalised_residuals.csv", 
-    #                     calc, type)
-    noise_vector<-read.csv(noise_read,header=FALSE,stringsAsFactors=FALSE)
-    NV<-c(noise_vector[1:20,1],noise_vector[33:38,1])
-    print(NV)
-} else if (noise_type!="exact" & noise_type!="extras") {
-  #noise_read = sprintf("/home/rach/Emulator/Noise_files/residual_variance/%s/nv_normal_res_%s_%s_%s.csv", 
-  #                     calc, type, multiplier, noise_type)
-  noise_read = sprintf("/home/rach/Emulator/Noise_files/residual_variance/%s/nv_res_%s_%s_%s.csv", 
-                       calc, type, multiplier, noise_type)
+if (noise_type!="exact" & noise_type!="extras") {
+  noise_read = sprintf("noise_files/nv_0424_%s_%s_%s.csv", 
+                                            calc, type, noise_type)
   noise_vector<-read.csv(noise_read,header=FALSE,stringsAsFactors=FALSE)
   NV<-c(noise_vector[1:20,1],noise_vector[33:38,1])
   NV
@@ -141,60 +124,60 @@ if (noise_type=="1mag" | noise_type=="2mag") {
 
 # Emulate ----------------------------------------------------------------------
 if (noise_type=="exact" | noise_type=="extras") {
-  EmModel <- km(formula=~., design=unit_training_input, response=training_output,
+  EmModel <- km(formula=~., design=training_input, response=training_output,
               covtype="matern5_2", nugget.estim=FALSE, optim.method="BFGS", control=list(maxit=500))
+} else if (noise_type=="r_nug") {
+  EmModel <- km(formula=~., design=training_input, response=training_output,
+                covtype="matern5_2", nugget.estim=TRUE, optim.method="BFGS", control=list(maxit=500))
 } else {
-  EmModel <- km(formula=~., design=unit_training_input, response=training_output,
+  EmModel <- km(formula=~., design=training_input, response=training_output,
                 covtype="matern5_2", noise.var=NV, optim.method="BFGS", control=list(maxit=500))
 }
 
 # Validate ---------------------------------------------------------------------
-validation_predictions <- predict(object=EmModel, newdata=data.frame(unit_validation_input),
+validation_predictions <- predict(object=EmModel, newdata=data.frame(validation_input),
                                   type="UK", checkNames=FALSE, light.return=TRUE)
 
 validation_plot(validation_output, validation_predictions)
 
 # Predict other values ---------------------------------------------------------
 # Predict at a set of grid values
-grid_values <- read.csv("/home/rach/Emulator/sample_vals.csv", header=FALSE, stringsAsFactors=FALSE)
+grid_values <- read.csv("misc/sample_vals.csv", header=FALSE, stringsAsFactors=FALSE)
+grid_values[,1] <- (grid_values[,1] - DesignRangesTable$Min[1])/(DesignRangesTable$Max[1] - DesignRangesTable$Min[1])
+grid_values[,2] <- (grid_values[,2] - DesignRangesTable$Min[2])/(DesignRangesTable$Max[2] - DesignRangesTable$Min[2])
+
 grid_predictions <- predict(object=EmModel, newdata=data.frame(grid_values),
                             type="UK", checkNames=FALSE, light.return=TRUE)
 
 # Predict at training points (for when a nugget term is added)
-training_predictions <- predict(object=EmModel, newdata = data.frame(unit_training_input), 
+training_predictions <- predict(object=EmModel, newdata = data.frame(training_input), 
                                 type="UK", checkNames=FALSE, light.return=TRUE)
 
 # Write to file ---------------------------------------------------------------
-if (noise_type=="exact" | noise_type=="extras" | noise_type=="1mag" | noise_type=="2mag" | noise_type=="trial") {
-  multiplier<-""
-} else {
-  multiplier<-sprintf("%s_", multiplier) # individual, mean, max 
-}
-
 saveRDS(EmModel,
-        file=sprintf("/home/rach/Emulator/The_Emulators/%s/%s_%s_%s%s", calc, calc, type, multiplier, noise_type))
+        file=sprintf("emulators/%s/%s_%s_%s", calc, calc, type, noise_type))
 
 write.csv(validation_predictions,
-          sprintf("/home/rach/Emulator/Predictions/%s/predict_validation_%s_%s_%s%s.csv", calc, calc, type, multiplier, noise_type),
+          sprintf("predictions/%s/pre_val_%s_%s_%s.csv", calc, calc, type, noise_type),
           row.names=FALSE,quote=FALSE)
 
 write.csv(grid_predictions,
-          sprintf("/home/rach/Emulator/Predictions/%s/predict_total_%s_%s_%s%s.csv", calc, calc, type, multiplier, noise_type),
+          sprintf("predictions/%s/pre_tot_%s_%s_%s.csv", calc, calc, type, noise_type),
           row.names=FALSE,quote=FALSE)
 
 write.csv(training_predictions,
-          sprintf("/home/rach/Emulator/Predictions/%s/predict_training_%s_%s_%s%s.csv", calc, calc, type, multiplier, noise_type),
+          sprintf("predictions/%s/pre_design_%s_%s_%s.csv", calc, calc, type, noise_type),
           row.names=FALSE,quote=FALSE)
 
-# Sensitivity analysis ---------------------------------------------------------
-distlist = rep('qunif',2)
-SA.model = fast99(model=krig.mean,factors=2,n=1000,q=(distlist),q.arg=list(min=0,max=1),m=EmModel)
-sample_direct = SA.model$y
-par(mfrow=c(1,1))
-plot(SA.model)
-
-sa_main  = SA.model$D1/SA.model$V
-sa_total = 1-SA.model$Dt/SA.model$V
-
-write.csv(sa_main,"/home/rach/Emulator/Predictions/sa_main_cf_teme.csv",row.names=FALSE,quote=FALSE)
-write.csv(sa_total,"/home/rach/Emulator/Predictions/sa_total_cf_teme.csv",row.names=FALSE,quote=FALSE)
+ # # Sensitivity analysis ---------------------------------------------------------
+# distlist = rep('qunif',2)
+# SA.model = fast99(model=krig.mean,factors=2,n=1000,q=(distlist),q.arg=list(min=0,max=1),m=EmModel)
+# sample_direct = SA.model$y
+# par(mfrow=c(1,1))
+# plot(SA.model)
+# 
+# sa_main  = SA.model$D1/SA.model$V
+# sa_total = 1-SA.model$Dt/SA.model$V
+# 
+# write.csv(sa_main,"predictions/sa_analysis/sa_main_cf_teme.csv",row.names=FALSE,quote=FALSE)
+# write.csv(sa_total,"predictions/sa_analysis/sa_total_cf_teme.csv",row.names=FALSE,quote=FALSE)
